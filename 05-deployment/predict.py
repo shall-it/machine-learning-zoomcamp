bb450dc822c0da806f1d047b3f0da8f98f472c91
Aleksei Shulga <ashulga@transperfect.com>
2025-10-22 02:32:59 +0300

    py scripts

---
+++ 05-deployment/predict.py
@@ -0,0 +1,45 @@
+
+
+# parameters
+
+model_file = 'model_C1.0.bin'
+
+
+# load the model
+
+with open(model_file, 'rb') as f_in:
+    dv, model = pickle.load(f_in)
+
+dv, model
+
+
+customer = {
+    'gender': 'female',
+    'seniorcitizen': 0,
+    'partner': 'yes',
+    'dependents': 'no',
+    'phoneservice': 'no',
+    'multiplelines': 'no_phone_service',
+    'internetservice': 'dsl',
+    'onlinesecurity': 'no',
+    'onlinebackup': 'yes',
+    'deviceprotection': 'no',
+    'techsupport': 'no',
+    'streamingtv': 'no',
+    'streamingmovies': 'no',
+    'contract': 'month-to-month',
+    'paperlessbilling': 'yes',
+    'paymentmethod': 'electronic_check',
+    'tenure': 1,
+    'monthlycharges': 29.85,
+    'totalcharges': 29.85
+}
+
+
+
+X = dv.transform([customer])
+
+y_pred = model.predict_proba(X)[0, 1]
+
+print('input:', customer)
+print('output:', y_pred)
