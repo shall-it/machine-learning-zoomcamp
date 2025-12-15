5e9470ed58b875bb983236798de0733eebab04d4
Alexey Shulga <26320355+shall-it@users.noreply.github.com>
2025-12-15 23:55:25 +0000

    app

---
+++ 10-kubernetes/app.py
@@ -0,0 +1,106 @@
+import os
+
+import numpy as np
+import onnxruntime as ort
+from keras_image_helper import create_preprocessor
+
+import pickle
+from typing import Literal
+from pydantic import BaseModel, Field
+
+from fastapi import FastAPI
+import uvicorn
+
+
+model_name = os.getenv("MODEL_NAME", "clothing-model.onnx")
+
+
+def preprocess_pytorch_style(X):
+    # X: shape (1, 299, 299, 3), dtype=float32, values in [0, 255]
+    X = X / 255.0
+
+    mean = np.array([0.485, 0.456, 0.406]).reshape(1, 3, 1, 1)
+    std = np.array([0.229, 0.224, 0.225]).reshape(1, 3, 1, 1)
+
+    # Convert NHWC → NCHW
+    # from (batch, height, width, channels) → (batch, channels, height, width)
+    X = X.transpose(0, 3, 1, 2)  
+
+    # Normalize
+    X = (X - mean) / std
+
+    return X.astype(np.float32)
+
+
+preprocessor = create_preprocessor(
+    preprocess_pytorch_style,
+    target_size=(224, 224)
+)
+
+
+
+session = ort.InferenceSession(
+    model_name, providers=["CPUExecutionProvider"]
+)
+input_name = session.get_inputs()[0].name
+output_name = session.get_outputs()[0].name
+
+classes = [
+    "dress",
+    "hat",
+    "longsleeve",
+    "outwear",
+    "pants",
+    "shirt",
+    "shoes",
+    "shorts",
+    "skirt",
+    "t-shirt",
+]
+
+
+def predict_single(url):
+    X = preprocessor.from_url(url)
+    result = session.run([output_name], {input_name: X})
+    float_predictions = result[0][0].tolist()
+    return dict(zip(classes, float_predictions))
+
+
+def lambda_handler(event, context):
+    url = event["url"]
+    result = predict_single(url)
+    return result
+
+
+
+class Request(BaseModel):
+    url: str = Field(..., example="http://bit.ly/mlbookcamp-pants")
+
+class PredictResponse(BaseModel):
+    predictions: dict[str, float]
+    top_class: str
+    top_probability: float
+
+app = FastAPI(title="clothing-model")
+
+@app.post("/predict")
+def predict(request: Request) -> PredictResponse:
+    predictions = predict_single(request.url)
+
+    top_class = max(predictions, key = predictions.get)
+    top_probability = predictions[top_class]
+
+    return PredictResponse(
+        predictions=predictions,
+        top_class=top_class,
+        top_probability=top_probability
+    )
+
+
+@app.get("/health")
+def health():
+    return {"status": "healthy"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
